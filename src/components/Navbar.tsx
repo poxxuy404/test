86ae7f8c9c8d0c675b9cfc54dda2caa2552159ea
poxxuy <bunyodqosimov48@gmail.com>
2026-06-01 13:39:08 +0500

    1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export function Navbar({ current }: { current?: string }) {
   return (
     <header className="navbar">
       <Link to="/" className="logo">
-        <img src="logo.png" alt="Logo" width="50" height="50" />
+        <img src="/logo.png" alt="Logo" width="50" height="50" />
       </Link>
       <nav className="nav-links">
       </nav>
